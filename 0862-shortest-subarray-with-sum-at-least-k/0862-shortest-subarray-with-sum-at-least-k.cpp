0ffcec8cb81201929a12664ca12e7e9b0e5ccace
Ashutosh kumar yadav <2k24.cs1f.2413882@gmail.com>
2026-08-20 23:16:29 +0530

    Time: 13 ms (75.35%), Space: 68.8 MB (52.25%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<long long> prefix(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        deque<int> dq;
+        int minLen = INT_MAX;
+
+        for (int i = 0; i <= n; i++) {
+
+            // sum >= k
+            while (!dq.empty() &&
+                   prefix[i] - prefix[dq.front()] >= k) {
+
+                minLen = min(minLen, i - dq.front());
+                dq.pop_front();
+            }
+
+            // Maintain increasing prefix sums
+            while (!dq.empty() &&
+                   prefix[i] <= prefix[dq.back()]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        return minLen == INT_MAX ? -1 : minLen;
+    }
+};
